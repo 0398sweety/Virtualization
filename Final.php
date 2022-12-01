38134b5aedf5b6f59cb98925a9b6bf3e14fc20fa
sweety parmar <39830606+0398sweety@users.noreply.github.com>
2022-11-30 20:52:03 -0500

    Update Final.php

--- Final.php
+++ Final.php
@@ -2,13 +2,16 @@
 <html>
 <body>
 
-<?php
-function writeMsg() {
-  echo "Hello world!";
-}
-
-writeMsg();
-?>
-
+    <h1>
+        Welcome....!!!!
+    </h1>
+    <p>Course : cloud computing</p>
+    <p>Subject : Virtualization</p>
+    <p>Project By :</p>
+    <ol>
+        <li>Prajakta Mangale</li>
+        <li>Sweety Parmar</li>
+        <li>Yatin Phullera</li>
+    </ol>
 </body>
 </html>
